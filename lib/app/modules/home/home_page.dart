a7b134d6ffe0ef6ba347227b7b2b0e4016569fcb
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-03-16 18:40:36 -0300

    Página Home completa

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
-import 'package:get/utils.dart';
-import 'package:oncoativ/app/core/ui/widgets/oncativ_textformfield.dart';
-import 'package:oncoativ/app/core/ui/widgets/oncoativ_appbar.dart';
-import 'package:oncoativ/app/core/ui/widgets/oncoativ_button.dart';
-import 'package:oncoativ/app/core/ui/widgets/oncoativ_button_desativa.dart';
-import 'package:oncoativ/app/core/ui/widgets/oncoativ_password.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,24 +12,390 @@ class HomePage extends StatelessWidget {
             Align(
               alignment: Alignment.topCenter,
               child: Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  SizedBox(
+                children: <Widget>[
+                  const SizedBox(
                     height: 52,
                   ),
-                  Text(
+                  const Text(
                     'Seja Bem Vindo',
                     style: TextStyle(color: Color(0xFFADA4A5), fontSize: 12),
                   ),
-                  Text(
+                  const Text(
                     'Nome do Usuário',
                     style: TextStyle(
                         color: Color(0xFF1D1617),
                         fontWeight: FontWeight.bold,
                         fontSize: 20),
                   ),
-                  SizedBox(
-                    height: 52,
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(.5),
+                            blurRadius: 20.0,
+                            spreadRadius: 0.0,
+                            offset: const Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: const LinearGradient(colors: [
+                          Color(0xFF9DCEFF),
+                          Color(0xFF89CF89),
+                        ]),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Row(
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                const Text(
+                                  'Não esqueça do treino',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text(
+                                  'Mantenha uma boa rotina',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                                const SizedBox(
+                                  height: 12,
+                                ),
+                                Container(
+                                  width: 100,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    gradient: const LinearGradient(
+                                        begin: Alignment.bottomLeft,
+                                        colors: [
+                                          Color(0x50029D6F),
+                                          Color(0xFF057755),
+                                        ]),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.transparent,
+                                      shadowColor: Colors.transparent,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                    child: const Text('Ver Treino'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: const <Widget>[
+                                Image(
+                                    image: AssetImage(
+                                        'assets/images/homem-correndo.png')),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(.5),
+                            blurRadius: 20.0,
+                            spreadRadius: 0.0,
+                            offset: const Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: const LinearGradient(colors: [
+                          Color(0xFFFFFFFF),
+                          Color(0xFFFFFFFF),
+                        ]),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Column(
+                              children: const <Widget>[
+                                SizedBox(
+                                  width: 60,
+                                  height: 60,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(2.0),
+                                    child: Image(
+                                      image: AssetImage(
+                                          'assets/images/especialista.png'),
+                                    ),
+                                  ),
+                                  // decoration: const BoxDecoration(
+                                  //     shape: BoxShape.circle,
+                                  //     color: Color(0xFFe0f2f1)),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Fale com especialista',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 4),
+                                  height: 28,
+                                  decoration: BoxDecoration(
+                                    gradient: const LinearGradient(
+                                        begin: Alignment.bottomLeft,
+                                        colors: [
+                                          Color(0xFF9DCEFF),
+                                          Color(0xFF89CF89),
+                                        ]),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.transparent,
+                                      shadowColor: Colors.transparent,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                    child: const Text(
+                                      'Falar Agora',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    margin: const EdgeInsets.only(bottom: 12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Acompanhe seu progresso',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Clique na opção abaixo para visualizar os exercícios realizados',
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(.5),
+                            blurRadius: 20.0,
+                            spreadRadius: 0.0,
+                            offset: const Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: const LinearGradient(colors: [
+                          Color(0xFFFFFFFF),
+                          Color(0xFFFFFFFF),
+                        ]),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 16, bottom: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const <Widget>[
+                                Text(
+                                  'Progresso Atual',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  height: 36,
+                                  decoration: BoxDecoration(
+                                    gradient: const LinearGradient(
+                                        begin: Alignment.bottomLeft,
+                                        colors: [
+                                          Color(0xFF9DCEFF),
+                                          Color(0xFF89CF89),
+                                        ]),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.transparent,
+                                      shadowColor: Colors.transparent,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                    child: const Text(
+                                      'Ver Mais',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(.5),
+                            blurRadius: 20.0,
+                            spreadRadius: 0.0,
+                            offset: const Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                        gradient: const LinearGradient(colors: [
+                          Color(0xFF9DCEFF),
+                          Color(0xFF89CF89),
+                        ]),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Row(
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                const Text(
+                                  'Calendário',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.5,
+                                  child: const Text(
+                                    'Clique para visualizar sua rotina de exercícios',
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 12,
+                                ),
+                                Container(
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    gradient: const LinearGradient(
+                                        begin: Alignment.bottomLeft,
+                                        colors: [
+                                          Color(0x50029D6F),
+                                          Color(0xFF057755),
+                                        ]),
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.transparent,
+                                      shadowColor: Colors.transparent,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                    child: const Text('Ver Histórico'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: const <Widget>[
+                                Image(
+                                    image: AssetImage(
+                                        'assets/images/calendario.png')),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
                   ),
                 ],
               ),
