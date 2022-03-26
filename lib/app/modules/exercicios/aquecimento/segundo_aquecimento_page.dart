2da40e48338534780d0956a4939e8efdc9ee597e
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-03-26 18:21:38 -0300

    Série 1 do Primeiro Aquecimento

---
+++ lib/app/modules/exercicios/aquecimento/segundo_aquecimento_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:oncoativ/app/core/ui/widgets/exercicios/Oncoativ_button_gradient_icon.dart';
+import 'package:oncoativ/app/core/ui/widgets/exercicios/oncoativ_button_som.dart';
+import 'package:oncoativ/app/core/ui/widgets/oncoativ_appbar.dart';
+import 'package:oncoativ/app/core/ui/widgets/exercicios/oncoativ_descricao_exercicio.dart';
+import 'package:oncoativ/app/core/ui/widgets/oncoativ_button.dart';
+
+class SegundoAquecimento extends StatelessWidget {
+  const SegundoAquecimento({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: OncoativAppBar(
+        title: const Text(
+          'Aquecimento 1',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: const Text(
+                            'Série 1 de 3',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 35),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: const Text(
+                            '8 repetições',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 1.0,
+                          height: MediaQuery.of(context).size.width * 1.0,
+                          child: const Image(
+                            image: AssetImage('assets/gifs/gifteste.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: OncoativButton(
+                          label: 'Descansar',
+                          onPressed: () {
+                            Get.toNamed('');
+                          }),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/app/routes/exercicios_routers.dart
+++ lib/app/routes/exercicios_routers.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:oncoativ/app/modules/exercicios/aquecimento/primeiro_aquecimento_page.dart';
+import 'package:oncoativ/app/modules/exercicios/aquecimento/segundo_aquecimento_page.dart';
 
 class ExerciciosRouters {
   ExerciciosRouters._();
 
   static final routers = <GetPage>[
     GetPage(name: '/exercicios/aquecimento/primeiroaquecimento', page: () => const PrimeiroAquecimento()),
+    GetPage(name: '/exercicios/aquecimento/segundoaquecimento', page: () => const SegundoAquecimento()),
   ];
 }
