311108564b0e77f72cf4adcaea6337c75ec3ca5c
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-03-26 21:37:42 -0300

    Component da tela de exercício

---
+++ lib/app/core/ui/widgets/exercicios/Oncoativ_exercicio_serie.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:oncoativ/app/core/ui/widgets/oncoativ_button.dart';
+
+class OncoativExercicioSerie extends StatelessWidget {
+  final String titulo;
+  final String subTitulo;
+  final String imagem;
+
+  const OncoativExercicioSerie({
+    Key? key,
+    required this.titulo,
+    required this.subTitulo,
+    required this.imagem,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                const SizedBox(
+                  height: 32,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Text(
+                    titulo,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Text(
+                    subTitulo,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 1.0,
+                  height: MediaQuery.of(context).size.width * 1.0,
+                  child: Image(
+                    image: AssetImage(imagem),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: OncoativButton(
+                  label: 'Descansar',
+                  onPressed: () {
+                    Get.toNamed(
+                        '/exercicios/aquecimento/terceiroaquecimentodescanso');
+                  }),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
