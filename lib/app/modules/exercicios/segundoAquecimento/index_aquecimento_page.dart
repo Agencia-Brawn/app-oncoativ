a2b422618421e14e840289aa3e69cf58e4fccf72
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-04-04 22:38:20 -0300

    Index aquecimento 2

---
+++ lib/app/modules/exercicios/segundoAquecimento/index_aquecimento_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:oncoativ/app/core/ui/widgets/Oncoativ_button_gradient_icon.dart';
+import 'package:oncoativ/app/core/ui/widgets/exercicios/oncoativ_button_som.dart';
+import 'package:oncoativ/app/core/ui/widgets/oncoativ_appbar.dart';
+import 'package:oncoativ/app/core/ui/widgets/exercicios/oncoativ_descricao_exercicio.dart';
+
+class IndexAquecimentoSegundo extends StatelessWidget {
+  const IndexAquecimentoSegundo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: OncoativAppBar(
+        title: const Text(
+          'Aquecimento 2',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  height: 16,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 1.0,
+                      height: MediaQuery.of(context).size.width * 0.9,
+                      child: const Image(
+                        image: AssetImage('assets/gifs/ombros_frente.gif'),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.width * 1.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          const Text(
+                            'Aquecimento: Movimento Circular  dos Ombros (para frente)',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          const OncoativDescricaoExercicio(
+                              textDescricao:
+                                  'Levante os ombros e dirija-os para frente realizando um grande movimento circular. Mantenha os braços soltos, se possível, durante toda realização do procedimento.'),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          const OncoativButtonIniciar(
+                              label: 'Aperte para esculta instruções'),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          // BUTTON INICIAR
+                          OncoativButtonGradientIcon(
+                              label: 'Iniciar Aquecimento',
+                              icon: const Icon(Icons.play_circle_fill_sharp),
+                              onPressed: () {
+                                Get.toNamed(
+                                    '/exercicios/segundoAquecimento/primeiroaquecimento');
+                              }),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
