5855abb32e033c13e8372e3e396f34077221c7b6
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-04-04 22:37:42 -0300

    Rota de vídeo

--- lib/app/routes/home_routers.dart
+++ lib/app/routes/home_routers.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
 import 'package:oncoativ/app/modules/exercicios/lista_exercicios_page.dart';
+import 'package:oncoativ/app/core/ui/widgets/exercicios/Oncoativ_video.dart';
 
 class HomeRouters {
   HomeRouters._();
 
+
   static final routers = <GetPage>[
     GetPage(name: '/exercicios/listaexercicios', page: () => const ListaExercicios()),
+    GetPage(name: '/exercicios/videoplayerapp', page: () => const OncoativVideo()),
   ];
 }
